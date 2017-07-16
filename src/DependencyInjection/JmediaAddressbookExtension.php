d9a770d04aae6d5da89da68d677ce40fa647b93a
j0nem <johannes@jonesmedia.de>
2017-07-16 17:31:41 +0200

    Register services.yml

---
+++ src/DependencyInjection/JmediaAddressbookExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Jmedia/AddressbookBundle
+ *
+ * @author Johannes Cram <johannes@jonesmedia.de>
+ * @package AddressbookBundle
+ * @license GPL-3.0+
+ */
+
+namespace Jmedia\AddressbookBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class JmediaAddressbookExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
