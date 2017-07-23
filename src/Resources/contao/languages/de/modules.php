8c7cf2960b1cddbb97660f2b5d46c6b930efe9ad
j0nem <johannes@jonesmedia.de>
2017-07-23 16:01:09 +0200

    Correctly display module name

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -16,5 +16,5 @@
  * @license GPL-3.0+
  */
 
- $GLOBALS['TL_LANG']['FMD']['fm_list'] = array('Adressbuch','Hier können Sie die Daten aller Mitglieder verwalten');
+ $GLOBALS['TL_LANG']['MOD']['fm_list'] = array('Adressbuch','Hier können Sie die Daten aller Mitglieder verwalten');
  $GLOBALS['TL_LANG']['MOD']['fm_verification'] = array('Verifikation','Hier können Sie neue Mitglieder, Anderungsvorschläge und neu hinzugefügte Adressbucheinträge überprüfen und freischalten');
