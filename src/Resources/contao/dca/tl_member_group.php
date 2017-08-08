65ed2aa55ada49ffca12cb04c41e215c8f06e500
j0nem <johannes@jonesmedia.de>
2017-08-08 21:21:23 +0200

    Add type field to tl_member_group

---
+++ src/Resources/contao/dca/tl_member_group.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Jmedia/AddressbookBundle
+ *
+ * @author Johannes Cram <johannes@jonesmedia.de>
+ * @package AddressbookBundle
+ * @license GPL-3.0+
+ */
+
+ //Add "group type" field to palettes
+ $GLOBALS['TL_DCA']['tl_member_group']['palettes'] = str_replace('{disable_legend}','{family_legend},family_group_type;{disable_legend}',$GLOBALS['TL_DCA']['tl_member_group']['palettes']);
+
+ //Add "group type" select field
+ $GLOBALS['TL_DCA']['tl_member_group']['fields']['family_group_type'] = array(
+	'label'         => &$GLOBALS['TL_LANG']['tl_member_group']['family_group_type'],
+	'exclude'		=> true,
+	'inputType'		=> 'select',
+	'options'		=> array('verified','unverified'),
+	'reference'		=> &$GLOBALS['TL_LANG']['tl_member_group']['family_group_type_options'],
+	'eval'			=> array('includeBlankOption' => true, 'tl_class'=>'w50','feEditable'=>true, 'feViewable'=>true),
+	'sql'			=> "varchar(255) NOT NULL default ''"
+ );
---
+++ src/Resources/contao/languages/de/tl_member_group.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Jmedia/AddressbookBundle
+ *
+ * @author Johannes Cram <johannes@jonesmedia.de>
+ * @package AddressbookBundle
+ * @license GPL-3.0+
+ */
+
+$GLOBALS['TL_LANG']['tl_member_group']['family_legend'] = 'Einstellungen für Familienadressbuch';
+$GLOBALS['TL_LANG']['tl_member_group']['family_group_type'] = array('Gruppenfunktion','Bitte geben Sie an, wenn nötig, ob diese Gruppe verifizierte oder nicht-verifizierte Mitglieder repräsentiert.');
+$GLOBALS['TL_LANG']['tl_member_group']['family_group_type_options'] = array(
+	'verified' 		=> 'Verifizierte Mitglieder',
+	'unverified' 	=> 'Nicht-verifiziete Mitglieder'
+);
