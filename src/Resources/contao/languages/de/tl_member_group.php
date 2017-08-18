b161fe0cf958744ba20f183c46fd114da0d377b4
j0nem <johannes@jonesmedia.de>
2017-08-18 14:16:27 +0200

    Add TODO comment

--- src/Resources/contao/languages/de/tl_member_group.php
+++ src/Resources/contao/languages/de/tl_member_group.php
@@ -16,6 +16,8 @@
  * @license GPL-3.0+
  */
 
+//TODO: Add check to avoid multiple "verified" and "unverified" groups
+
 $GLOBALS['TL_LANG']['tl_member_group']['family_legend'] = 'Einstellungen für Familienadressbuch';
 $GLOBALS['TL_LANG']['tl_member_group']['family_group_type'] = array('Gruppenfunktion','Bitte geben Sie an, wenn nötig, ob diese Gruppe verifizierte oder nicht-verifizierte Mitglieder repräsentiert.');
 $GLOBALS['TL_LANG']['tl_member_group']['family_group_type_options'] = array(
