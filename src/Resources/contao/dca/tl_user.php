7603daeffcd0d62ddb709b8f3befebf1b8c923b7
j0nem <johannes@jonesmedia.de>
2017-08-18 16:15:01 +0200

    Add family notification field for backend users

---
+++ src/Resources/contao/dca/tl_user.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Jmedia/AddressbookBundle
+ *
+ * @author Johannes Cram <johannes@jonesmedia.de>
+ * @package AddressbookBundle
+ * @license GPL-3.0+
+ */
+
+ //Add field "family notification" to palettes
+ $GLOBALS['TL_DCA']['tl_user']['palettes'] = str_replace('admin;','admin,family_notifications;',$GLOBALS['TL_DCA']['tl_user']['palettes']);
+
+ //Add field "family notifications" to fields
+ $GLOBALS['TL_DCA']['tl_user']['fields']['family_notifications'] = array(
+	'label'         => &$GLOBALS['TL_LANG']['tl_user']['family_notifications'],
+	'exclude'		=> true,
+	'inputType'		=> 'checkbox',
+	'sql'			=> "char(1) NOT NULL default ''"
+ );
---
+++ src/Resources/contao/languages/de/tl_user.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+/**
+ * Jmedia/AddressbookBundle
+ *
+ * @author Johannes Cram <johannes@jonesmedia.de>
+ * @package AddressbookBundle
+ * @license GPL-3.0+
+ */
+
+ $GLOBALS['TL_LANG']['tl_user']['family_notifications'] = array('Adressbuch-Benachrichtigungen','Bitte wählen Sie, ob der angegebene Backend-Benutzer E-Mail Benachrichtigungen erhalten soll, wie z.B. über neue Mitglieder-Registrierungen.');
